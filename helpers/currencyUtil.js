27ecc1751985fed3e5efee99dce163d9ddff53bd
Scaccoman <scaccoman@gmail.com>
2018-02-08 14:51:31 +0000

    add query validation and split currency helper

---
+++ helpers/currencyUtil.js
@@ -0,0 +1,42 @@
+var request = require("request"),
+    fs      = require("fs");
+const ROOT_URL = "https://openexchangerates.org/api/latest.json?app_id=";
+const API_KEY  = process.env.API_KEY || "f8e1c9cbfc18487ca598143236557288";
+
+exports.requestRates = function(callback){
+        request(ROOT_URL + API_KEY, function(error, response, body){
+            if (!error && response.statusCode == 200){
+                //get useful data out of json
+                const rates = JSON.parse(body).rates;
+                rates.USD = 1;
+                //store rates and timestamp in json files
+                fs.writeFile( "./data/rates.json", JSON.stringify(rates), "utf8", function(){
+                    console.log("rates stored successfully");
+                    fs.writeFile( "./data/ratesAge.json", JSON.stringify(new Date().getTime()), "utf8", function(){
+                        console.log("age stored successfully");
+                     });
+                });
+                callback(null, rates);
+            } else {
+                callback(error);
+            }
+        });
+};
+
+exports.calcExchange = function(base, target, rates, amount){
+    const valueInUsd = (1 / rates[base]) * amount;
+    const valueInTargetCurrency = valueInUsd * rates[target];
+    return valueInTargetCurrency;
+};
+
+exports.validateQuery = function(base, target, amount) {
+    if (base.match("^[A-Z]{3}$") 
+        && target.match("^[A-Z]{3}$") 
+        && amount.toString().match("^[0-9]*$")) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+module.exports = exports;
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+var express    = require('express'),
+    app        = express(),
+    port       = process.env.PORT || 3000,
+    bodyParser = require('body-parser'),
+    morgan     = require('morgan');
     
 var apiRoutes = require("./routes/currency");
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname +'/public'));
-app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-    res.sendFile("index.html");
+    res.send("Exchange rate server online");
 });
 
 app.use('/api/', apiRoutes);
 
 app.listen(port, function(){
-    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
-})
-    
-    
+    console.log("Exchange rate relay server started on port: " + process.env.PORT);
+});
