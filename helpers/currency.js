c37fb9abdf2a7e168378c38ef6e698816201588e
Scaccoman <scaccoman@gmail.com>
2018-02-08 15:36:15 +0000

    add stats management

--- helpers/currency.js
+++ helpers/currency.js
@@ -2,27 +2,31 @@ const fs      = require("fs"),
       util    = require("./currencyUtil");
 
 exports.getRates = function(req, res){
-    const base = req.query.base;
-    const target = req.query.target;
-    const amount = req.query.amount;
+    const base   = req.query.base,
+          target = req.query.target,
+          amount = req.query.amount;
     
     //check if query is valid
     if (util.validateQuery(base, target, amount)){
         const age = JSON.parse(fs.readFileSync('./data/ratesAge.json', 'utf8'));
         const now = new Date().getTime();
 
-        //check if the exchange rates are older then 60 minutes
+        //if the exchange rates are older then 60 minutes, get new rates
         if (now >= age + 3600000) {
             util.requestRates(function(err, rates) {
                 if (err) return res.send(err);
                 const result = util.calcExchange(base, target, rates, amount);
                 res.send(JSON.stringify(result));
             });
+        //otherwise read stored rates
         } else if (now < age + 3600000) {
             const rates = JSON.parse(fs.readFileSync('./data/rates.json', 'utf8'));
             const result = util.calcExchange(base, target, rates, amount);
             console.log(result);
+            
+            //validate output
             if (result.toString().match("^[0-9]*[.]{1}[0-9]*$|^[0-9]*$")){
+                util.saveStats(target, result);
                 res.send(JSON.stringify(result));
             } else {
                 res.send("Invalid base or target currency!");
@@ -34,6 +38,7 @@ exports.getRates = function(req, res){
 };
 
 exports.getIndex = function(req, res){
+    util.resetStats();
     res.send("Exchange rate server online");
 };
 
--- helpers/currencyUtil.js
+++ helpers/currencyUtil.js
@@ -6,7 +6,7 @@ const API_KEY  = process.env.API_KEY || "f8e1c9cbfc18487ca598143236557288";
 exports.requestRates = function(callback){
         request(ROOT_URL + API_KEY, function(error, response, body){
             if (!error && response.statusCode == 200){
-                //get useful data out of json
+                //get rates data out of json
                 const rates = JSON.parse(body).rates;
                 rates.USD = 1;
                 //store rates and timestamp in json files
@@ -22,6 +22,26 @@ exports.requestRates = function(callback){
             }
         });
 };
+
+exports.saveStats = function(target, amount){
+    const stats = JSON.parse(fs.readFileSync('./data/stats.json', 'utf8'));
+    stats.currencies[target]++;
+    stats.total += amount;
+    fs.writeFile( "./data/stats.json", JSON.stringify(stats), "utf8", function(){
+        console.log("stats stored successfully");
+    });
+};
+
+// exports.resetStats = function(){
+//     const stats = JSON.parse(fs.readFileSync('./data/stats.json', 'utf8'));
+//     Object.keys(stats.currencies).forEach(function(key){
+//         stats.currencies[key] = 0;
+//     });
+//     stats.total = 0;
+//     fs.writeFile( "./data/stats.json", JSON.stringify(stats), "utf8", function(){
+//         console.log("stats reset successfully");
+//     });
+// };
 
 exports.calcExchange = function(base, target, rates, amount){
     const valueInUsd = (1 / rates[base]) * amount;
